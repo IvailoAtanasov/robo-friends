2b956445c5a13480764e5ac7a312e479342b3cba
IvailoAtanasov <ivailoatan@gmail.com>
2021-11-04 17:42:39 +0200

    create Cards, CardList, SearchField, and State

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from "react";
+import CardList from "./CardList";
+import SearchBox from "./SearchBox";
+import "./App.css";
+
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchfield: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
+  render() {
+    const filteredRobots = this.state.robots.filter((robots) => {
+      return robots.name
+        .toLocaleLowerCase()
+        .includes(this.state.searchfield.toLocaleLowerCase());
+    });
+    if (this.state.robots.length === 0) {
+      return <h1>Loading...</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox
+            searchField={this.searchField}
+            searchChange={this.onSearchChange}
+          />
+          <CardList robots={filteredRobots} />
+        </div>
+      );
+    }
+  }
+}
+
+export default App;
